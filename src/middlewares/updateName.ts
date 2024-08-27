db8244bb296b3f7018d58be96c460e73e76dfd86
Alex Kraudov <alexkraudov@MacBook-Air-Alex.local>
2024-08-27 10:35:32 +0300

    g

--- src/middlewares/updateName.ts
+++ src/middlewares/updateName.ts
@@ -1,16 +1,33 @@
 import { Middleware } from "grammy";
 import { MyContext } from "../typings/context";
 import { User } from "../models/user";
+import { Group } from "../models/group";
 
 export const updateName: Middleware<MyContext> = async (ctx, next) => {
   let user = await User.findOne({ id: ctx.from?.id });
+  let group = await Group.findOne({ id: ctx.chat?.id });
   
   await next();
-  
-  if (!user?.first_name) {
+
+  if (user?.first_name && user.username){
+    if (user.first_name.slice(0, 30) !== ctx.from?.first_name || user.username !== ctx.from.username) {
+      await User.updateOne({ id: ctx.from?.id }, 
+        { first_name: ctx.from?.first_name.slice(0, 30),
+          username: ctx.from?.username
+        })
+    }
+  } else {
     await User.updateOne({ id: ctx.from?.id }, 
       { first_name: ctx.from?.first_name.slice(0, 30),
         username: ctx.from?.username
-       })
+      })
   }
+
+  if (group?.title)
+    if (group.title.slice(0, 30) !== ctx.chat?.title || group.username !== ctx.chat?.username){
+      await Group.updateOne({ id: ctx.from?.id }, 
+        { title: ctx.from?.first_name.slice(0, 30),
+          username: ctx.from?.username
+        })
+    }
 } 
