8185a3e9732582808327a970ee1f28c4a1632b19
Alex Kraudov <alexkraudov@MacBook-Air-Alex.local>
2024-08-27 10:53:03 +0300

    g

--- src/middlewares/updateName.ts
+++ src/middlewares/updateName.ts
@@ -12,22 +12,25 @@ export const updateName: Middleware<MyContext> = async (ctx, next) => {
   if (user?.first_name && user.username){
     if (user.first_name.slice(0, 30) !== ctx.from?.first_name || user.username !== ctx.from.username) {
       await User.updateOne({ id: ctx.from?.id }, 
-        { first_name: ctx.from?.first_name.slice(0, 30),
+        { 
+          first_name: ctx.from?.first_name.slice(0, 30),
           username: ctx.from?.username
         })
     }
   } else {
     await User.updateOne({ id: ctx.from?.id }, 
-      { first_name: ctx.from?.first_name.slice(0, 30),
+      { 
+        first_name: ctx.from?.first_name.slice(0, 30),
         username: ctx.from?.username
       })
-  }
-
+    }
+  
   if (group?.title && ctx.chat?.title)
     if (group.title.slice(0, 30) !== ctx.chat?.title || group.username !== ctx.chat?.username){
       await Group.updateOne({ id: ctx.chat?.id }, 
-        { title: ctx.chat.title.slice(0, 30),
-          username: ctx.chat.username
+        { 
+          title: ctx.chat.title.slice(0, 30),
+          username: ctx.chat.username ? ctx.chat.username : null
         })
     }
 } 
